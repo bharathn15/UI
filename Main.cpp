2891fdfb388b9fff5cc4ca6ccc613376d6da1405
bharathn15 <bharath.bglr.15@gmail.com>
2022-01-23 01:57:06 +0530

    Coloring the window

--- Main.cpp
+++ Main.cpp
@@ -11,12 +11,15 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(700, 700, "GLFW-Window", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(700, 700, "GLFW-OpenGL Window", NULL, NULL);
 	glfwMakeContextCurrent(window);
 
 	gladLoadGL();
 
 	glViewport(0,0, 700,700);
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
 
 	if (window == NULL) {
 		cout<< "Failed to initialise window." << endl;
